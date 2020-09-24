1e23bc7442c1baa00231e65d779c716c173da6e8
Kenty <>
2020-09-24 18:55:16 +0900

    refactored register

--- NomiGuin/Controller/RegisterUserViewController.swift
+++ NomiGuin/Controller/RegisterUserViewController.swift
@@ -6,22 +6,33 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterUserViewController: UIViewController {
-
+    
+    var ref: DatabaseReference!
+    
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var mailAddressTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
+
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func registerBtnWasPressed() {
-        FirebaseAuthAPI().registerUser(username: userNameTextField.text!, email: mailAddressTextField.text!, password: passwordTextField.text!)
+        Auth.auth().createUser(withEmail: mailAddressTextField.text!, password: passwordTextField.text!) { (authResult, error) in
+            if let err = error{
+                print("ERROR: \(err)")
+            }else{
+                guard let userID = authResult?.user.uid else { return }
+                self.ref.child("users").child(userID).setValue(["username": self.userNameTextField.text!, "totalAlcoholValue": 0])
+            }
+        }
     }
     
 }
